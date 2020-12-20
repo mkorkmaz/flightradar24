6409a4c2bfcc51ed502707768c85d5c4868ba295
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2020-12-20 16:52:37 +0300

    setup updated

---
+++ long_description.rst
@@ -0,0 +1,8 @@
+flightradar24
+================
+
+Data library for Flightadar24.com written in Python 3.
+
+If you want to use  the data collected using this library commercially, You need to subscribe to Business Plan. See details at  https://www.flightradar24.com/premium/
+
+See on Github for details https://github.com/mkorkmaz/flightradar24
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('long_description.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -13,12 +13,13 @@ setup(
     name='flightradar24',
     version='0.3',
     description='Data library for Flight Radar 24',
+    long_description_content_type='text/x-rst',
     long_description=readme,
-    long_description_content_type="text/markdown",
     author='Mehmet Korkmaz',
     author_email='mehmet@mkorkmaz.com',
     url='https://github.com/mkorkmaz/flightradar24',
     license=package_license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=["requests"]
 )
 
